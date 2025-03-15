b96bc54aaf72741d78f256c4d1440efffa1be108
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2025-03-15 18:05:07 +0300

    feat(genritiveAI): add image picker functionality and update dependencies in pubspec.yaml and pubspec.lock

--- lib/features/GenritiveAI/presintation/page/PageFour.dart
+++ lib/features/GenritiveAI/presintation/page/PageFour.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 import 'package:hikayati_app/core/util/ScreenUtil.dart';
 import 'package:hikayati_app/features/Regestrion/date/model/userMode.dart';
@@ -19,6 +21,7 @@ class PageFour extends StatefulWidget {
 
 class _PageFourState extends State<PageFour> {
   ScreenUtil screenUtil = ScreenUtil();
+  String? selectedImagePath;
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +33,12 @@ class _PageFourState extends State<PageFour> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            CircleAvatar(
-              maxRadius: 30,
-              backgroundColor: Colors.white,
-              child: Image.asset(Assets.images.logo.path),
-            ),
-            Text('حكايتي', style: AppTheme.textTheme.displaySmall),
+            // CircleAvatar(
+            //   maxRadius: 30,
+            //   backgroundColor: Colors.white,
+            //   child: Image.asset(Assets.images.logo.path),
+            // ),
+            // Text('حكايتي', style: AppTheme.textTheme.displaySmall),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -74,37 +77,85 @@ class _PageFourState extends State<PageFour> {
                       type: TextInputType.text,
                     ),
 
-
-
-                    Text('قم برفع صورة  بطل قصتك:',
+                    Text('قم برفع صورة بطل قصتك:',
                         style: AppTheme.textTheme.displaySmall),
                     SizedBox(
                       height: screenUtil.screenHeight * .05,
                     ),
-                    CustomField(
-                      size: 200,
-                      onching: (value) {
-                        controller.userModel!.user_name = value.toString();
-                        controller.update();
-                      },
-                      valdution: (value) {
-                        if (value.toString().isEmpty) {
-                          return 'يرجى منك ادخال اسم الطفل ';
-                        } else if (!RegExp(
-                            r"^([\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z]+)$")
-                            .hasMatch(value)) {
-                          return "لا يمكن ان يحتوي اسم الطفل على ارقام او رموز";
-                        }
-                        return null;
-                      },
-                      controler:
-                      controller.name, // Use a TextEditingController here
-                      text: 'مثال  :  محمد',
-                      type: TextInputType.text,
+                    Container(
+                      width: 200,
+                      child: InkWell(
+                        onTap: () async {
+                          final ImagePicker _picker = ImagePicker();
+                          final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+                          
+                          if (image != null) {
+                            setState(() {
+                              selectedImagePath = image.path;
+                            });
+                            showImagesDialog(
+                              context,
+                              Assets.images.Characters.hasham.happy.path,
+                              'تم اختيار الصورة بنجاح',
+                              () {
+                                Navigator.pop(context);
+                              },
+                            );
+                          }
+                        },
+                        child: selectedImagePath != null
+                            ? Container(
+                                height: 100,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: AppTheme.primaryColor,
+                                    width: 3,
+                                  ),
+                                ),
+                                child:CircleAvatar(
+                                  backgroundImage: FileImage(
+                                    File(selectedImagePath!),
+
+                                  ),
+                                ),
+                              )
+                            : Container(
+                                height: 100,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    width: 3,
+                                    color: AppTheme.primarySwatch.shade200,
+                                  ),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.add_photo_alternate,
+                                      color: AppTheme.primaryColor,
+                                      size: 30,
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      'اختر صورة',
+                                      style: TextStyle(
+                                        fontFamily: AppTheme.fontFamily,
+                                        color: AppTheme.primaryColor,
+                                        fontSize: 10,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                      ),
                     ),
                   ],
                 ),
-
               ],
             ),
           ],
