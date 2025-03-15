f2b680055301683e421a1e23ef3211b616352f2a
Eng.Abdu khalid yahya Al_hrazi <82823586+almomyz@users.noreply.github.com>
2025-03-16 01:51:14 +0300

    feat(genritiveAI): add GenritiveAIPage widget for the GenritiveAI feature

---
+++ lib/features/GenritiveAI/presintation/page/GenritiveAIPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GenritiveAIPage extends StatefulWidget {
+  const GenritiveAIPage({super.key});
+
+  @override
+  State<GenritiveAIPage> createState() => _GenritiveAIPageState();
+}
+
+class _GenritiveAIPageState extends State<GenritiveAIPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Container();
+  }
+}
